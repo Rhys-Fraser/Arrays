c66e1a8ed910265190734b5deee808fc491a6615
RhysFraser <116083655+Rhys-Fraser@users.noreply.github.com>
2022-11-08 13:37:17 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int myAge = 25;
+        int yourAge = 27;
+        int suziesAge = 23;
+        int bobbysAge = 34;
+        int jimsAge = 36;
+        Console.WriteLine(myAge);
+        Console.WriteLine(yourAge);
+        Console.WriteLine(suziesAge);
+        Console.WriteLine(bobbysAge);
+        Console.WriteLine(jimsAge);
+    }
+}
