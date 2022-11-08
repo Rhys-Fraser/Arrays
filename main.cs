3a9bb06b34788e0b9038f257ef7f729e6d48227f
RhysFraser <116083655+Rhys-Fraser@users.noreply.github.com>
2022-11-08 13:52:40 +0000

    simplified code

--- main.cs
+++ main.cs
@@ -4,15 +4,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-        int myAge = 25;
-        int yourAge = 27;
-        int suziesAge = 23;
-        int bobbysAge = 34;
-        int jimsAge = 36;
-        Console.WriteLine(myAge);
-        Console.WriteLine(yourAge);
-        Console.WriteLine(suziesAge);
-        Console.WriteLine(bobbysAge);
-        Console.WriteLine(jimsAge);
+        //int[] ages = new int[] { 25, 27, 23, 34, 36 };
+        //Console.WriteLine(25, 27, 23, 34, 36);
+        int[] ages = new int[5];
+        ages[0] = 25;
+        ages[1] = 27;
+        ages[2] = 23;
+        ages[3] = 34;
+        ages[4] = 36;
+        int totalAge = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(ages[i]);
+            totalAge += ages[i];
+        }
+        //int totalAge = myAge + yourAge + suziesAge + bobbysAge + jimsAge;
+        //Console.WriteLine(totalAge);
     }
 }
